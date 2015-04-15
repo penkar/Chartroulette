2d07d95173fabec207f5927010a5297ef8a259dc
jsp <jspenkar@gmail.com>
2015-04-15 18:17:04 -0500

    still trying to get the highchart to mount through react properly

--- components/app.js
+++ components/app.js
@@ -1,13 +1,16 @@
-// var chart = new Highcharts.Chart({
-//     chart: {
-//         renderTo: 'container'
-//     },
+var createChart = function(mount){
+	var chart = new Highcharts.Chart({
+		chart: {
+			renderTo: mount
+		},
+		series: [{
+			data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+		}]
+	});
+	_charts.push(chart);
+}
 
-//     series: [{
-//         data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
-//     }]
-
-// });
+var _charts = []
 
 var App = React.createClass({
 	displayName: "App",
@@ -20,13 +23,20 @@ var App = React.createClass({
 })
 
 var Chart = React.createClass({
+	getInitialState: function(){
+		return {count:0}
+	},
+	increment: function(){
+		this.setState({count: (this.state.count + 1)})
+	},
 	displayName: 'Chart',
 	render: function(){
-		return React.createElement(Highcharts.Chart, {
-			series: [{
-		        data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
-		    }]			
-		})
+		var mount = 'mount'+this.state.count;
+		this.increment;
+		return React.createElement('div', { className: mount } )
+	},
+	componentDidUpdate: function(){
+		createChart('mount0')
 	}
 })
 
@@ -34,7 +44,7 @@ var ChartContainer = React.createClass({
 	displayName: 'ChartContainer',
 	render: function(){
 		return React.createElement('div', {className:'container'}, 
-			' '// React.createElement(chart, null)
+			React.createElement(Chart, null)
 		)
 	}
 })
