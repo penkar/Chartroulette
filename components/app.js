0b12cf009afce87b9a2fe991b9898b5bfba766ec
jsp <jspenkar@gmail.com>
2015-04-16 17:03:05 -0500

    Debugging

--- components/app.js
+++ components/app.js
@@ -1,4 +1,4 @@
-var _charts = ['a','b','c']
+var _charts = ['a','b','c'];
 
 var App = React.createClass({
 	displayName: "App",
@@ -6,13 +6,26 @@ var App = React.createClass({
 		var len = this.state._charts.length;
 		var current = ((this.state.current + np)+len) % len;
 		this.setState({current: current});
-		console.log(this.state);
+	},
+	subtract: function(){
+		var charts = this.state._charts;
+		var len = charts.length-1;
+		var current = this.state.current;
+		console.log( charts );
+		console.log( len );
+		console.log( current	 );
+		charts.splice(current,1);
+		current = (current + len)%len;
+		this.setState({
+			current:current,
+			_charts: charts
+		})
 	},
 	getInitialState: function(){
 		return({
 			current: 0,
 			_charts: _charts
-		})
+		});
 	},
 	render: function(){
 		return React.createElement('div', {className: 'container'},
@@ -20,27 +33,13 @@ var App = React.createClass({
 				current:this.state.current,
 				_charts: this.state._charts
 			}),
-			React.createElement(Nav, {scroll: this.scroll})
-		)
+			React.createElement(Nav, {
+				scroll: this.scroll,
+				subtract: this.subtract
+			})
+		);
 	}
-})
-
-var Chart = React.createClass({
-	getInitialState: function(){
-		return {
-			count:0,
-			chartCount:[0]
-		}
-	},
-	displayName: 'Chart',
-	render: function(){
-		var mount = 'mount'+this.props.count;
-		return React.createElement('div', { id: mount, className: 'chart '+this.props.class } );
-	},
-	componentDidMount: function(){
-		createChart('mount'+this.props.count);
-	}
-})
+});
 
 var ChartContainer = React.createClass({
 	displayName: 'ChartContainer',
@@ -62,12 +61,27 @@ var ChartContainer = React.createClass({
 			array
 		)
 	}
-})
+});
+
+var Chart = React.createClass({
+	getInitialState: function(){
+		return {
+			count:0,
+			chartCount:[0]
+		}
+	},
+	displayName: 'Chart',
+	render: function(){
+		var mount = 'mount'+this.props.count;
+		return React.createElement('div', { id: mount, className: 'chart '+this.props.class } );
+	},
+	componentDidMount: function(){
+		createChart('mount'+this.props.count);
+	}
+});
+
 var Nav = React.createClass({
 	displayName: 'Nav',
-	scroll: function(np){
-		this.props.scroll(np);
-	},
 	render: function(){
 		return React.createElement('div', null,
 			React.createElement('input', {type:'button', onClick:this.onClick, value: 'Prev'}),
@@ -84,6 +98,7 @@ var Nav = React.createClass({
 			case 'Add':
 				break;
 			case 'Sub':
+				this.props.subtract()
 				break;
 			case 'Next':
 				this.props.scroll(1);
@@ -91,5 +106,4 @@ var Nav = React.createClass({
 			default:
 		}
 	}
-})
-
+});
