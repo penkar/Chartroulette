541018e19d270c3f36eb6889b3c3bf4759a6a027
jsp <jspenkar@gmail.com>
2015-04-18 23:45:21 -0500

    Comments added to app.js and chartlogic.js

--- components/chartlogic.js
+++ components/chartlogic.js
@@ -1,8 +1,9 @@
+// The Create chart method will mount a Highchart in the component did mount lifecycle event of the react component.
 var createChart = function(mount, series){
 	var series = randomData()
-	document['mount'+mount] = new Highcharts.Chart({
+	document['mount'+mount] = new Highcharts.Chart({ //The Chart itself will be available here through the document on the console. You will be able to manipulate the chart and data here.
 		chart: { 
-			renderTo: 'mount'+mount,
+			renderTo: 'mount'+mount, //The component will mount to a div created in the React component. While you can destroy the chart, you can't destroy the div without messing up the chart orders.
 			type: chartTypes()
 		},
 		series,
@@ -15,6 +16,7 @@ var chartTypes = function(){
 	return types[Math.floor(Math.random()*types.length)]
 }
 
+// Random dummy data for the generic charts. 
 var randomData = function(){
 	var array = [];
 	for(var i = 0; i < 15; i++){
@@ -24,6 +26,7 @@ var randomData = function(){
 	return series
 }
 
+// Scroll Function is necessary on the global scope for the charts to function properly.
 var scrollFunc = function(current){
 	document.getElementsByClassName('current')[0].className = "chart"
 	document.getElementById('mount'+current).className = 'chart current'
