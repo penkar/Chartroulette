6271e82d8b724c44d73e7df87cd8bcbdc1a8f52e
jsp <jspenkar@gmail.com>
2015-04-22 15:47:15 -0500

    sub and add functions being removed from app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 var CLogic = require('./chartlogic.js');
 var Header = require('./components/header.js');
 var Nav = require('./components/nav.js');
-var ChartStore = require('./store/chartstore.js');
 var ChartContainer = require('./components/chartcontainer.js');
+var ChartStore = require('./store/chartstore.js');
 var CurrentStore = require('./store/currentstore.js');
 
 var App = React.createClass({
@@ -14,34 +14,46 @@ var App = React.createClass({
 			_charts: ChartStore.getAll()
 		});
 	},
-	sub: function(){
-		// Subtract chart will run the Highcharts destroy method on the current chart, scroll to the next chart, and remove the current chart from the _charts list which is used for scrolling between charts. 
-		var current = this.state.current;
-		var _charts = this.state._charts;
-		var len = _charts.length;
-		if(len === 1){
-			return false
-		};
-		var idx = _charts.indexOf(current);
-		document['mount'+current].destroy(); //React JS will not rerender the Highcharts correctly. Since you have to create the highchart after its container has been rendered, when the charts rerender they do properly read the highchart. In order to get around this you have to attach the chart globally and delete it through the global scope. 
-		current = _charts[(idx + 1)%len];
-		_charts.splice(idx,1); // Method will remove current view from available views. 
-		this.setState({
-			current: current,
-			_charts: _charts
-		});
-		this.scroll(1);
+	componentDidMount: function() {
+		ChartStore.addChangeListener(this._onChange);
 	},
-	add: function(){// This method does 3 things: 
-		var _charts = this.state._charts;
-		var next = this.state.next;
-		_charts.push(next);
+	componentWillUnmount: function() {
+		ChartStore.removeChangeListener(this._onChange);
+	},
+	_onChange: function() {
 		this.setState({
-			_charts: _charts, //Adds the new index to the list of available charts. 
-			next: (next+1), //  Sends the recent state to the chart container which will in turn create a new chart.
-			current: next //  Changes the current view to thew newly created chart. 
+			_charts: ChartStore.getAll(),
+			next: CurrentStore.getNext()
 		});
 	},
+	// sub: function(){
+	// 	// Subtract chart will run the Highcharts destroy method on the current chart, scroll to the next chart, and remove the current chart from the _charts list which is used for scrolling between charts. 
+	// 	var current = this.state.current;
+	// 	var _charts = this.state._charts;
+	// 	var len = _charts.length;
+	// 	if(len === 1){
+	// 		return false
+	// 	};
+	// 	var idx = _charts.indexOf(current);
+	// 	document['mount'+current].destroy(); //React JS will not rerender the Highcharts correctly. Since you have to create the highchart after its container has been rendered, when the charts rerender they do properly read the highchart. In order to get around this you have to attach the chart globally and delete it through the global scope. 
+	// 	current = _charts[(idx + 1)%len];
+	// 	_charts.splice(idx,1); // Method will remove current view from available views. 
+	// 	this.setState({
+	// 		current: current,
+	// 		_charts: _charts
+	// 	});
+	// 	this.scroll(1);
+	// },
+	// add: function(){// This method does 3 things: 
+	// 	var _charts = this.state._charts;
+	// 	var next = this.state.next;
+	// 	_charts.push(next);
+	// 	this.setState({
+	// 		_charts: _charts, //Adds the new index to the list of available charts. 
+	// 		next: (next+1), //  Sends the recent state to the chart container which will in turn create a new chart.
+	// 		current: next //  Changes the current view to thew newly created chart. 
+	// 	});
+	// },
 	scroll: function(np){
 		var _charts = this.state._charts;
 		var current = this.state.current;
@@ -59,9 +71,9 @@ var App = React.createClass({
 				next: this.state.next
 			}),
 			React.createElement(Nav, {
-				scroll: this.scroll, //Previous and next are not needed, the scroll function passes a +1 / -1 depending on the direction. 
-				sub: this.sub,
-				add: this.add
+				scroll: this.scroll//, //Previous and next are not needed, the scroll function passes a +1 / -1 depending on the direction. 
+				// sub: this.sub,
+				// add: this.add
 			})
 		);
 	}
