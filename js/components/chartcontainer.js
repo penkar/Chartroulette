c4a2d45bfeb7300e5ffc489fb305c5d7206214f7
jsp <jspenkar@gmail.com>
2015-04-24 21:11:08 -0500

    Refactored some code.

--- js/components/chartcontainer.js
+++ js/components/chartcontainer.js
@@ -1,4 +1,5 @@
 var Chart = require('./chart.js');
+var ChartStore = require('../store/chartstore.js');
 var CurrentStore = require('../store/currentstore.js');
 
 var ChartContainer = React.createClass({
@@ -10,39 +11,28 @@ var ChartContainer = React.createClass({
 			current: CurrentStore.getCurrent()
 		});
 	},
-	createSingle: function(i){
-		var current = '';
-		if(!i){ // On the first run through when the page is generated this will establish chart 0 as the current chart.
-			current = 'current';
-		};
-		return React.createElement(Chart, {class: current, id: i, key:i}); //Method will create a single chart each go around. This way it can be called on the initial render and each time new props are received.
-	},
-	componentWillMount: function(){
-		var array = []; //The first go around this will run and create a new chart from 0 up to recent. 
-		var next = this.props.next
-		for(var i = 0; i < next; i++){
-			array.push( this.createSingle(i) );
-		}
-		this.setState({charts: array})
-	},
 	render: function(){
-		var arr = [];
-		for(var i = 0; i < this.state.next; i++){
-			arr.push( this.createSingle(i));
+		var arr = [], charts = this.state.charts;
+		for(var i = 0; i < charts.length; i++){
+			var id = charts[i];
+			arr.push( React.createElement(Chart, {class: current, id: id, key:id}));
 		}
 		return React.createElement('div', {className:'chart-container'}, 
 			arr
 		)
 	},
 	componentDidMount: function() {
+		ChartStore.addChangeListener(this._onChange);
 		CurrentStore.addChangeListener(this._onChange);
 	},
 	componentWillUnmount: function() {
+		ChartStore.removeChangeListener(this._onChange);
 		CurrentStore.removeChangeListener(this._onChange);
 	},
 	_onChange: function() {
 		this.setState({
-			next: CurrentStore.getNext()
+			next: CurrentStore.getNext(),
+			charts: ChartStore.getAll()
 		});
 	}
 });
